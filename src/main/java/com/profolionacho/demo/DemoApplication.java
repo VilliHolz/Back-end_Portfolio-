920a4c7ca4f09d7c5714a5df78b6186155d3b2d3
Ignacio Nicolas Villagra Holz <nachoprogramando@gmail.com>
2022-08-28 18:45:44 -0300

    Agregada URL Front

--- src/main/java/com/profolionacho/demo/DemoApplication.java
+++ src/main/java/com/profolionacho/demo/DemoApplication.java
@@ -20,7 +20,7 @@ public class DemoApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://ignacio-villagra-holz.web.app"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Acces-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type","Accept", "Authorization", "Acces-Control-Allow-Origin", "Acces-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
